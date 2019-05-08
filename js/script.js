39412476de8b4dbcfef09182f1c4f899b126c5ff
Ana uyehara <ana.uyehara0@gmail.com>
2019-05-07 19:42:17 -0700

    myhero branch

--- js/script.js
+++
@@ -1,9 +0,0 @@
-console.log(formatName (userFirstName, userLastName));
-
-function formatName (firstName, lastName){
-    var formattedName = lastName + ' ' + firstName;
-    localStorage.setItem('userFullName', formattedName);
-
-    return formattedName;
-    
-}
